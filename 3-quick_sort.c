af6ffad0683f7c7b700a91217a8f27dc65fb56f8
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-08-07 22:15:22 -0500

    Feature: added needed files for exercise 3-quicksort

---
+++ 3-quick_sort.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an arry using the quick sort algorithm
+ * @array: the array to sort
+ * @size: the size of array
+ */
+void quick_sort(int *array, size_t size)
+{
+}
